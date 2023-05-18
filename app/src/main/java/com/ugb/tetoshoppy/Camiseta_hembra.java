ad056901c080803ec985c2496113dc2e94872903
MultiConversor <cuentagithub07@gmail.com>
2023-05-18 12:16:52 -0600

    agregacion de menu y de 2 xml con informacion de productos

---
+++ app/src/main/java/com/ugb/tetoshoppy/Camiseta_hembra.java
@@ -0,0 +1,14 @@
+package com.ugb.tetoshoppy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Camiseta_hembra extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.camiseta_hembra);
+    }
+}
---
+++ app/src/main/java/com/ugb/tetoshoppy/camisas_main.java
@@ -0,0 +1,14 @@
+package com.ugb.tetoshoppy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class camisas_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.camisas_main);
+    }
+}
